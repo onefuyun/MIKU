325be1c939f79b95f0298a300953577f31eb0e1a
onefuyun <2300253102@qq.com>
2018-05-07 22:58:16 +0800

    测试两个git 账号，改了异步的路由

--- src/router/index.js
+++ src/router/index.js
@@ -9,31 +9,32 @@ import Search from '../pages/search.vue'
 import Detail from '../pages/detail.vue'
 import Rookie from '../pages/rookie.vue'
 import LoadApp from '../pages/loadApp.vue'
-import NoCancle from '../pages/nocancle.vue'
+// import NoCancle from '../pages/nocancle.vue'
 
 const routes = [
-  { path: '/',component:Index,beforeEnter(to,from,next){
-      if(to.path === '/'){
-         next("/home")  
-      }else{
-         next()
+  {
+    path: '/', component: Index, beforeEnter(to, from, next) {
+      if (to.path === '/') {
+        next("/home")
+      } else {
+        next()
       }
+    },
+    children: [
+      { path: 'home', component: Home },
+      { path: 'sort', component: Sort },
+      { path: 'store', component: Store },
+      { path: 'earnings', component: Earnings },
+      { path: 'user', component: User },
+
+    ]
   },
-  children:[
-            { path: 'home', component: Home},
-            { path: 'sort', component: Sort},
-            { path: 'store', component: Store},
-            { path: 'earnings', component: Earnings},
-            { path: 'user', component: User},
-           
-     ]
-  },
-  {path:'/search',component:Search},
-  {path:'/detail',component:Detail},
-  {path:'/rookie',component:Rookie},
-  {path:'/loadapp',component:LoadApp},
-  {path:'/nocancle',component:NoCancle}
+  { path: '/search', component: Search },
+  { path: '/detail', component: Detail },
+  { path: '/rookie', component: Rookie },
+  { path: '/loadapp', component: LoadApp },
+  { path: '/nocancle', components: resolve => { require(["../pages/nocancle.vue"], resolve) }}
   ]
-  
+
 
 export default routes
